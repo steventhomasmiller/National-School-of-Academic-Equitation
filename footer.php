00c484d03f98abffe1dae5debb4f11b328a5a1cc
Chris Badger <cbadger44@gmail.com>
2014-10-04 00:19:42 -0700

    whitespace and syntax
    $args was mistyped as $ars

--- footer.php
+++ footer.php
@@ -1,16 +1,11 @@
 	<footer class="site-footer">
-
-		<nav class="site-nav"><!-- Footer Navigation -->
+		<nav class="site-nav">    <!-- Footer Navigation -->
 			<?php
-				$ars = array(
-						'theme_location' => 'footer'
-					);
+				$args = array( 'theme_location' => 'footer' );
 				?>
 			<?php wp_nav_menu( $args ); ?>
 		</nav>
-
-		<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
-
+			<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
 	</footer>
 
 </div><!-- container -->
