244d7f3d090a81f682abdf88822ca5d14f0d3113
cluetrain <greenstrat67@gmail.com>
2014-10-14 20:41:31 -0700

    Beginning to add sidebars

--- functions.php
+++ functions.php
@@ -8,10 +8,34 @@ function wordpress_resources() {
 
 add_action('wp_enqueue_scripts', 'wordpress_resources');
 
-//Register Navigation Menus Here
+//Navigation Menu
 register_nav_menus(array(
 	'primary' => __( 'Primary Menu' ),
 	'footer' => __( 'Footer Menu' ),
 
-	//test comment
 ));
+
+//Sidebars
+
+if ( function_exists('register_sidebar') ) {
+register_sidebar(array(
+'before_widget' => '<li id="%1$s" class="widget %2$s">',
+'after_widget' => '</li>',
+'before_title' => '<h2 class="widgettitle">',
+'after_title' => '</h2>',
+));
+}
+
+//Second sidebar
+
+if ( function_exists('register_sidebar') ) {
+register_sidebar(array(
+'name' => 'Homepage Sidebar',
+'id' => 'homepage-sidebar',
+'description' => 'Appears as the sidebar on the custom homepage',
+'before_widget' => '<div style="height: 280px"></div><li id="%1$s" class="widget %2$s">',
+'after_widget' => '</li>',
+'before_title' => '<h2 class="widgettitle">',
+'after_title' => '</h2>',
+));
+}
