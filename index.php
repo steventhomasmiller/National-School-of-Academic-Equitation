a5a7ce4fdc28fc0d130b14e11714a4b450b0f07a
Steve Miller <steventhomasmiller@gmail.com>
2014-10-23 20:58:33 -0700

    Index updates

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
 
-get_header();
+<div class="wrapper">
 
-if (have_posts()) :
-	while (have_posts()) : the_post(); ?>
+	get_header();
 
-	<article class="post">
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<?php the_content(); ?>
-	</article>
+	if (have_posts()) :
+		while (have_posts()) : the_post(); ?>
 
-	<?php endwhile;
+		<article class="post">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php the_content(); ?>
+		</article>
 
-	else : 
-		echo '<p>No content found</p>';
-	endif;
+		<?php endwhile;
 
-get_footer();
+		else : 
+			echo '<p>No content found</p>';
+		endif;
+		?>
+
+	<?php get_sidebar("right"); ?>
+
+	get_footer();
+
+</div>
 
 ?>
 
-<?php get_sidebar("right"); ?>
+
