36d5ea57d736881b919236ab8f3f47f904581a16
Steve Miller <steventhomasmiller@gmail.com>
2014-10-30 20:56:22 -0700

    Thomas just helped with some of the styles that weren't computing.

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <div class="blank-divider"></div>
 <footer class="container-fluid">
+
+	
+
 	<div id="footer-address">
 		<ul>
 			<li><h5>The National School of Academic Equitation</h5></li>
@@ -7,6 +10,8 @@
 			<li>Snohomish, WA 98296</li>
 			<li>nsae@classical-equitation.com</li>
 		</ul>
+
+
 				<nav class="site-nav-foot">    <!-- Footer Navigation -->
 					<?php
 						$args = array( 'theme_location' => 'footer' );
@@ -14,6 +19,7 @@
 					<?php wp_nav_menu( $args ); ?>
 				</nav>
 					<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+
 
 	</div>	
 
