b317da67e1ebc81be903927763117cd6a572ef0e
Steve Miller <steventhomasmiller@gmail.com>
2014-11-16 22:18:52 -0800

    Embedded map and contact form in Contact template

--- page-contact.php
+++ page-contact.php
@@ -38,10 +38,10 @@ get_header(); ?>
 
     <div class="col-md-8">
         <?php echo do_shortcode("[ic_add_posts ids='49']");?> 
-        <p> there is a call for get_post(id) once we have the id for the map inside a col-md-8</p>
+        <p><?php echo '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2679.913438437759!2d-122.07241210000001!3d47.8025137!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490099fa8cb1b87%3A0x58fdfaa57191842b!2s11901+215th+Pl+SE%2C+Snohomish%2C+WA+98296!5e0!3m2!1sen!2sus!4v1415999981823" width="600" height="450" frameborder="0" style="border:0"></iframe>';?> </p>
     </div>
     <div class="col-md-4">
-        <?php get_post(); ?><p> there is a call for the form post here, or hand code the form here</p>
+        <?php get_post(); ?><p> <?php echo do_shortcode("[contact-form-7 id='48']"); ?></p>
     </div>
 
 </div>
