5ba864c3d24d80e6f731ee95363c8c883c9f6880
cbadger44 <chris_badger1@msn.com>
2014-11-18 10:07:24 -0800

    navigation menu
    found a way to get it to drop down correctly, still needs styling of
    course

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 			<a href="<?php bloginfo("url")?>"><img class="header" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Main photo" /></a></h1>
 			<a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
 		<h5><?php bloginfo('description'); ?></h5>
-			<nav class="site-nav"><!-- ***** Header Navigation ***** -->
+			<nav id="main-nav"><!-- ***** Header Navigation ***** -->
 				<?php
 					$args = array( 'theme_location' => 'primary' );
 				?>
