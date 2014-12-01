3a2e1d238e64b4270fbe926dbd95ad911c9198a1
Steve Miller <steventhomasmiller@gmail.com>
2014-11-30 21:03:40 -0800

    Adding News page template

---
+++ page-news.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: News Template
+*/
+get_header(); ?>
+
+<?php 
+// the query
+$the_query = new WP_Query( 'category_name=news' ); ?>
+
+<?php if ( $the_query->have_posts() ) : ?>
+
+	<!-- pagination here -->
+
+	<!-- the loop -->
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<span class="date"><?php the_time('F jS, Y') ?></span></h4>
+		<span class="excerpt"><?php the_excerpt(); ?></span>
+	<?php endwhile; ?>
+	<!-- end of the loop -->
+
+	<!-- pagination here -->
+
+	<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
