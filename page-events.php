646d18bc930cc7bef2e5cac8e4cd67c2591b6f37
cbadger44 <chris_badger1@msn.com>
2014-12-09 15:16:41 -0800

    tweak

--- page-events.php
+++ page-events.php
@@ -14,7 +14,7 @@ $the_query = new WP_Query( 'category_name=events' ); ?>
 
 	<!-- the loop -->
 	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-		<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		<span class="excerpt"><?php the_excerpt(); ?></span>
 	<?php endwhile; ?>
 	<!-- end of the loop -->
--- page-news.php
+++ page-news.php
@@ -14,7 +14,7 @@ $the_query = new WP_Query( 'category_name=news' ); ?>
 
 	<!-- the loop -->
 	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>&#771;&nbsp;
 		<span class="date"><?php the_time('F jS, Y') ?></span></h3>
 		<span class="excerpt"><?php the_excerpt(); ?></span>
 	<?php endwhile; ?>
