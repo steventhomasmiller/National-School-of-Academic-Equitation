9ac202932139fcea860103d31178007626701038
cbadger44 <chris_badger1@msn.com>
2014-12-09 15:09:46 -0800

    excerpt

--- page-events.php
+++ page-events.php
@@ -15,7 +15,7 @@ $the_query = new WP_Query( 'category_name=events' ); ?>
 	<!-- the loop -->
 	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		<span class="excerpt"><p><?php the_excerpt(); ?></p></span>
+		<span class="excerpt"><?php the_excerpt(); ?></span>
 	<?php endwhile; ?>
 	<!-- end of the loop -->
 
