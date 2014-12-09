c5cdf1ac7659c00d5aad6757487242453a48d4d1
cbadger44 <chris_badger1@msn.com>
2014-12-09 14:51:37 -0800

    events

---
+++ page-events.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Events Template
+*/
+get_header(); ?>
+
+<?php 
+// the query
+$the_query = new WP_Query( 'category_name=events' ); ?>
+
+<?php if ( $the_query->have_posts() ) : ?>
+
+	<!-- pagination here -->
+
+	<!-- the loop -->
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<span class="excerpt"><?php the_excerpt(); ?></span>
+	<?php endwhile; ?>
+	<!-- end of the loop -->
+
+	<!-- pagination here -->
+
+	<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
